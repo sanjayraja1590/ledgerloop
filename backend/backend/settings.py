05493d48fcc42fd356611a10c95d263b7cde20c8
sanjayraja1590 <sanjayraja1590@gmail.com>
2026-01-14 13:06:56 +0530

    Use Postgres instead of SQLite

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,12 +57,17 @@ TEMPLATES = [
 WSGI_APPLICATION = "backend.wsgi.application"
 
 # ---------------- DATABASE ----------------
+import dj_database_url
+import os
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
+
 
 # ---------------- PASSWORDS ----------------
 AUTH_PASSWORD_VALIDATORS = [
